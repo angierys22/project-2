e7d8feb921d9ec2f4c5e9dddae2510a812ef4f5a
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-09 21:26:29 -0500

    first attempt

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS workout_db;
+CREATE DATABASE workout_db;
+USE workout_db;
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    PRIMARY KEY (id)
+);
+CREATE TABLE exercise (
+    id INT NOT NULL AUTO_INCREMENT,
+    muscle_group VARCHAR(50),
+    equipment VARCHAR(50),
+    name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+CREATE TABLE journal (
+    id INT NOT NULL AUTO_INCREMENT,
+    createdAt TIMESTAMP NOT NULL,
+    text VARCHAR(255),
+    user_id INT NOT NULL,
+    exercise_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
