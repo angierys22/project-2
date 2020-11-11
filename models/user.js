ffad001bbdb2673461eda983d0a1eda6958d20da
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-11 01:41:30 -0500

    initial commit

---
+++ models/user.js
@@ -0,0 +1,22 @@
+module.exports = function (sequlize, DataTypes) {
+   const User = sequlize.define("User", {
+      firstName: {
+         type: DataTypes.STRING,
+         allowNull: false,
+
+      },
+      lastName: {
+         type: DataTypes.STRING,
+         allowNull: false,
+
+      },
+      eMail: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         validate: {
+            isEmail: true,
+         },
+      }
+   });
+   return User;
+};
