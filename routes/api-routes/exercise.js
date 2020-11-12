59208564568fdd02e1d00cf1e5d2c647a05e80d6
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-11 20:32:51 -0500

    initial commit

---
+++ routes/api-routes/exercise.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../models");
+
+router.get("/",(req,res,next) => {
+   db.Exercise.findAll({})
+      .then((results)=> {
+         res.json(results);
+      })
+      .catch( (err) => {
+         res.status(500);
+         next(err);
+      });
+});
+
+module.exports = router;
---
+++ routes/api-routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../models");
+
+router.get("/",(req,res,next) => {
+   db.User.findAll({})
+      .then((results)=> {
+         res.json(results);
+      })
+      .catch( (err) => {
+         res.status(500);
+         next(err);
+      });
+});
+
+module.exports = router;
