a7c40e94fc0eea2f46659ed4cd95b4cf45ec79b0
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-14 11:29:40 -0500

    adding routes

--- routes/api-routes/journal.js
+++ routes/api-routes/journal.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../models");
 
+//Get all posts
 router.get("/",(req,res,next) => {
    db.Journal.findAll({})
       .then((results)=> {
@@ -12,5 +13,29 @@ router.get("/",(req,res,next) => {
          next(err);
       });
 });
+
+//Get posts by user id
+router.get("/:id",(req,res,next)=>{
+   db.Journal.findAll({
+      where: {
+         UserId: req.params.id,
+
+      }
+   })
+      .then((results)=>{
+         res.json(results);
+      })
+      .catch((err)=>{
+         res.status(500);
+         next(err);
+      });
+});
+
+//Post a new entry
+// router.post("/",(req,res,next)=>{
+//    db.Journal.create({
+
+//    });
+// });
 
 module.exports = router;
--- routes/api-routes/version.js
+++ routes/api-routes/version.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const db = require("../../models");
 
-// /api/commnet/create 
-// post comment route -> back to index
 router.get("/", (req, res) => {
    const version = { version: "0.0.1", requestURL: req.url };
    res.json(version);
 
 });
-
-// router.post("/create", (req, res, next) => {
-//    db.Test.create(req.body)
-//       .then(newComment => {
-//          console.log(newComment);
-//          res.redirect("/");
-//          // optionally return data created
-//          // res.json(newComment)
-//       })
-//       .catch(err => {
-//          res.status(500);
-//          next(err);
-//       });
-// });
 
 module.exports = router;
