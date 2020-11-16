95d69aa4ec8499aca6009f20bdff2443068e58e5
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-16 00:25:13 -0500

    fixed file name

---
+++ models/exercise.js
@@ -0,0 +1,18 @@
+module.exports = function (sequelize, DataTypes) {
+   const Exercise = sequelize.define("Exercise", {
+      name: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      muscleGroup: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      equipment: {
+         type: DataTypes.STRING,
+
+      }
+   },
+   {timestamps: false});
+   return Exercise;
+};
