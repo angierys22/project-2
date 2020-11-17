1a42adae690fb936e830db396e7743928b8584a9
Angelica Reyes <areyes1293@gmail.com>
2020-11-16 21:19:04 -0500

    updated exercise page

--- routes/html-routes/main.js
+++ routes/html-routes/main.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { resolveConfig } = require("prettier");
+const db = require("../../models");
 const router = express.Router();
 // const db = require("../../models");
 
@@ -13,8 +15,20 @@ router.get("/gym", (req, res) => {
 router.get("/JE", (req, res) => {
    res.render("JE");
 });
-router.get("/exercise", (req, res) => {
-   res.render("exercise");
+router.get("/exercise/:equipment/:id", (req, res) => {
+   db.Exercise.findAll({
+      where:{
+        muscleGroup: req.params.id, 
+        equipment: req.params.equipment
+      }
+   })
+   .then ((exercises)=>{
+      let muscles= req.params.id 
+      let equipment= req.params.equipment
+      muscles= muscles.charAt(0).toUpperCase() + muscles.slice(1);
+      equipment= equipment.charAt(0).toUpperCase() + equipment.slice(1);
+      res.render("exercise", {exercises:exercises, muscleGroup:muscles, equipment:equipment});
+   })
 });
 // // get comments from db and send to template
 // db.Test.findAll({})
